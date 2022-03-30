7fd23ded13634ed10d995b27bd333f2529426032
James Martini <jamesmartini1008@gmail.com>
2022-03-30 13:43:51 -0400

    lots of changes
    living room finished and lighting is more or less better (a lot of the lights are just not working for some reason but idk why)

---
+++ Podcast Game/Assets/Scripts/DoorOpen.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DoorOpen : MonoBehaviour
+{
+    bool open = false;
+    float speed = 1f;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (Keyboard.current.eKey.wasPressedThisFrame)
+            {
+                StartCoroutine(OpenDoor());
+            }
+        }
+    }
+
+    IEnumerator OpenDoor()
+    {
+        if (!open)
+        {
+            Debug.Log("OPENING");
+            // Rotate the door to -90 degrees on the y
+            float t = 0.0f;
+            Vector3 doorStart = transform.localRotation.eulerAngles;
+            Vector3 doorEnd = new Vector3(0, -90, 0);
+            while (t < 1.0f)
+            {
+                t += Time.deltaTime * speed * 2;
+                transform.localRotation = Quaternion.Euler(Vector3.Lerp(doorStart, doorEnd, t));
+                yield return null;
+            }
+
+            open = true;
+        }
+        else
+        {
+            Debug.Log("CLOSING");
+            // Rotate the door to 0 degrees on the y
+            float t = 0.0f;
+            Vector3 doorStart = transform.localRotation.eulerAngles;
+            Vector3 doorEnd = new Vector3(0, 0, 0);
+            while (t < 1.0f)
+            {
+                t += Time.deltaTime * speed * 2;
+                transform.localRotation = Quaternion.Euler(Vector3.Lerp(doorStart, doorEnd, t));
+                yield return null;
+            }
+
+            open = false;
+        }
+    }
+}
